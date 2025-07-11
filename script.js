a6f8104efc6df162780a65ba48b756fcdd482412
Flávio Félix <fau_felix30@hotmail.com>
2025-07-11 18:41:36 -0300

    Done struct IA

--- script.js
+++ script.js
@@ -5,9 +5,64 @@ const askButton = document.getElementById("askButton");
 const aiResponse = document.getElementById("aiResponse");
 const form = document.getElementById("form");
 
-const enviarFormulario = (e) => {
+// AIzaSyDY4D9Ut3fb7cMkaIlY8HYNnnfuM3Xh5sI
+const perguntaAi = async (question, game, apiKey) => {
+  const model = "gemini-2.5-flash";
+  const geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+  const pergunta = `
+    olha, tenho esse jogo: ${game} e queria saber ${question}
+  `;
+  const contents = [
+    {
+      parts: [
+        {
+          text: pergunta,
+        },
+      ],
+    },
+  ];
+  // Chamada API
+  const response = await fetch(geminiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents,
+    }),
+  });
+
+  const data = await response.json();
+
+  return data.candidates[0].contents.parts[0].text;
+};
+
+const enviarFormulario = async (e) => {
   e.preventDefault();
-  console.log("Formulário enviado");
+  const apiKey = apiKeyInput.value;
+  const game = gameSelect.value;
+  const question = questionInput.value;
+
+  if (apiKey == "" || game == "" || question == "") {
+    alert("Preencha todos os campos para enviar a sua pergunta.");
+    return;
+  }
+
+  askButton.disabled = true;
+  askButton.textContent = "Perguntando...";
+  askButton.classList.add("loading");
+
+  try {
+    // pergunta para IA
+    const text = await perguntaAi(question, game, apiKey);
+    aiResponse.querySelector(".response-content").innerHTML = text;
+  } catch (error) {
+    console.log("Error: ", error);
+  } finally {
+    askButton.disabled = false;
+    askButton.textContent = "Perguntar";
+    askButton.classList.remove("loading");
+  }
 };
 
 form.addEventListener("submit", enviarFormulario);
